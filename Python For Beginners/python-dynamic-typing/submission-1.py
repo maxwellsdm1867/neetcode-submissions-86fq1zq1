21c52dac4795492ec79cfd720b40ee6aca2fb936
arthurh <49833088+maxwellsdm1867@users.noreply.github.com>
2026-05-18 17:39:46 -0700

    Add: python-dynamic-typing - submission-1

---
+++ Python For Beginners/python-dynamic-typing/submission-1.py
@@ -0,0 +1,10 @@
+variable = 6
+print(type(variable))
+variable = 6.1
+print(type(variable))
+variable = False
+print(type(variable))
+variable = 'hi'
+print(type(variable))
+variable = [1, 2, 3]
+print(type(variable))
